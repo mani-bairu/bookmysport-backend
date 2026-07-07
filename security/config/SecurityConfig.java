378ad7bc02af0ac5d81f1676c9b9dbd72d665c02
mani-bairu <157734821+mani-bairu@users.noreply.github.com>
2026-07-07 18:20:39 +0800

    added Swagger and Actuator to the project

--- security/config/SecurityConfig.java
+++ security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -29,7 +30,7 @@ public class SecurityConfig {
     JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public BCryptPasswordEncoder pwdEncoder(){
+    public PasswordEncoder pwdEncoder(){
         return new BCryptPasswordEncoder();
     }
 
@@ -57,6 +58,16 @@ public class SecurityConfig {
 
                     req.requestMatchers("/register", "/login").permitAll()
                             .requestMatchers("/ws/**","/test/**").permitAll()
+                            .requestMatchers(
+                                    "/swagger-ui/**",
+                                    "/v3/api-docs/**",
+                                    "/swagger-ui.html"
+                            ).permitAll()
+                            .requestMatchers(
+                                    "/swagger-ui/**",
+                                    "/v3/api-docs/**",
+                                    "/actuator/health"
+                            ).permitAll()
                             .anyRequest().authenticated();
                 })
                 .addFilterBefore(
