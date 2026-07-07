e518cdcbb63b90a237babeb82a119fe33b005a3c
mani-bairu <157734821+mani-bairu@users.noreply.github.com>
2026-07-07 20:45:24 +0800

    Integrated Async for email service

--- notification/service/EmailService.java
+++ notification/service/EmailService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 //@Service
@@ -44,6 +45,7 @@ public class EmailService {
     private final JavaMailSender mailSender;
 
 
+    @Async
     public void sendHtmlEmail(
             String to,
             String subject,
